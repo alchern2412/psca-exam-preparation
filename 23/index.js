c21bc3772abcebc872a30c9c3b3c25bcdcec0a6c
alchern2412 <mynameislyosha2412@yandex.com>
2020-06-15 09:31:27 +0300

    Add 23 (pipe)

---
+++ 23/index.js
@@ -0,0 +1,21 @@
+// 23.	Применение функции pipe для обработки данных (файла)
+// файловой системы  и записи в http-ответ. Пример.
+
+let http = require('http');
+let fs = require('fs');
+//Pipe - это канал, который связывает поток 
+//для чтения и поток для записи и позволяет сразу 
+//считать из потока чтения в поток записи. 
+const handler = (req, res) => {
+    if (req.method == 'GET') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain; charset=utf-8',
+            // 'Content-Disposition': 'form-data; filename="file.txt"'
+        });
+        fs.createReadStream('./file.txt').pipe(res);
+    }
+}
+
+http.createServer().listen(3000)
+    .on('error', (e) => { console.log('error: ', e.code) })
+    .on('request', handler);
